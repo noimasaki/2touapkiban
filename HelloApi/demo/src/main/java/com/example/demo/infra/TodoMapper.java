671a2bd2a3fd8baa5eed419b052584066bcda71d
noimasaki <89688362+noimasaki@users.noreply.github.com>
2026-08-16 07:01:17 +0000

    xmlでfindAllできた

--- HelloApi/demo/src/main/java/com/example/demo/infra/TodoMapper.java
+++ HelloApi/demo/src/main/java/com/example/demo/infra/TodoMapper.java
@@ -8,22 +8,29 @@ import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.demo.domain.Todo;
 
 
 @Mapper
 public interface TodoMapper {
-    @Select("SELECT id, title, done FROM todo ORDER BY id")
+    // @Select("SELECT id, title, done FROM todo ORDER BY id")
+    // List<Todo> findAll();
     List<Todo> findAll();
 
-    @Select("SELECT id, title, done FROM todo WHERE id = #{id}")
+    // @Select("SELECT id, title, done FROM todo WHERE id = #{id}")
+    // Optional<Todo> findById(Long id);
     Optional<Todo> findById(Long id);
 
-    @Insert("INSERT INTO todo(title, done) VALUES(#{title}, false)")
+    // @Insert("INSERT INTO todo(title, done) VALUES(#{title}, false)")
+    // @Options(useGeneratedKeys = true, keyProperty = "id")
+    // void insert(Todo todo);
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void insert(Todo todo);
 
-    @Update("UPDATE todo SET done = #{done} WHERE id = #{id}")
-    int update(Long id, Boolean done);
+
+    // @Update("UPDATE todo SET done = #{done} WHERE id = #{id}")
+    // int update(Long id, Boolean done);
+    int update(@Param("id") Long id, @Param("done") boolean done);
 }
